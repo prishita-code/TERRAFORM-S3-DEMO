4056c4e2c89a791378b8643b112355cac91840d5
prishita-code <prishitasingh8@gmail.com>
2025-08-22 10:49:55 +0100

    Commit main.tf

--- main.tf
+++ main.tf
@@ -1,1 +1,32 @@
+terraform {
+    required_providers {
+        aws = {
+        source  = "hashicorp/aws"
+        version = "~> 5.0"
+        }
+        random = {
+        source  = "hashicorp/random"
+        version = "~> 3.0"
+        }
+    }
+}
+provider "aws" {
+    region = var.aws_region
+}
 
+resource "random_id" "bucket_id" {
+    byte_length = 8
+}
+resource "aws_s3_bucket" "my_bucket" {
+    bucket = "my-unique-bucket-${random_id.bucket_id.hex}"
+
+    tags = {
+        Name        = "My terraform test bucket"
+    }
+    force_destroy = true
+}
+
+output "My terraform test bucket" { 
+    value = aws_s3_bucket.my_bucket.bucket  
+  
+}
